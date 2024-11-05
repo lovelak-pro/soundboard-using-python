e08cc42d403122d102ef587b78fda5a099a426fe
lovelak-pro <123636043+lovelak-pro@users.noreply.github.com>
2024-11-05 12:40:18 +0500

    removed an extra py file

--- main.py
+++ main.py
@@ -17,6 +17,38 @@ print('\n\tYour Soundboard is Ready to use.')
 
 
 
+fbi_path = 'sfx\\fbi-open-up-sfx.mp3'
+anime_ahh_path = 'sfx\\anime-ahh.mp3'
+anime_wow_path = 'sfx\\anime-wow-sound-effect.mp3'
+ara_ara_path = 'sfx\\ara-ara.mp3'
+emotional_damage_path = 'sfx\\emotional-damage-meme.mp3'
+ew_brother_ew_path = 'sfx\\eww-brother-eww.mp3'
+yes_omg_remix_path = 'sfx\\yes-omg-remix.mp3'
+yes_mommy_path = 'sfx\\yes-mommy.mp3'
+yes_daddy_path = 'sfx\\yes-daddy.mp3'
+yesyes_nono_path = 'sfx\\yes-yes-yes-yes-no-no-no-no.mp3'
+yamate_kudasai_path = 'sfx\\yamate-kudasai.mp3'
+weeeee_path = 'sfx\\weeeee_original_1193597514938524841.mp3'
+noob_path = 'sfx\\noob sound.mp3'
+byebye_path = 'sfx\\byebye-indian.mp3'
+the_rizz_path = 'sfx\\the-weekend-rizz-extended.mp3'
+hub_intro_path = 'sfx\\hub-intro-sound.mp3'
+im_sponge_bob_path = 'sfx\\im-spongebob.mp3'
+limit_on_talking_path = 'sfx\\limit-on-talking.mp3'
+sorry_indian_path = 'sfx\\sorry-indian.mp3'
+spiderman_meme_path = 'sfx\\spiderman-meme-song.mp3'
+dog_laughing_path = 'sfx\\laughing-dog-meme.mp3'
+sad_violin_path = 'sfx\\worlds-smallest-violin.mp3'
+what_the_dog_doin_path = 'sfx\\what-the-dog-doin.mp3'
+snore_mimimi_path = 'sfx\\snore-mimimimimimi.mp3'
+meme_ending_path = 'sfx\\meme-de-creditos-finales.mp3'
+run_meme_path = 'sfx\\run-vine-sound-effect.mp3'
+gay_echo_path = 'sfx\\gay-echo.mp3'
+thankyou_anime_path = 'sfx\\thankyou_anime.mp3'
+thankyou_myfriend_path = 'sfx\\thank_you_my_friend.mp3'
+
+
+
 def fbi():
     mixer.music.load(fbi_path)
     mixer.music.play()
